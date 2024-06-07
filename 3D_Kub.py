214b0291d5e0b4a6f7b013c1fd3b39bba76a829e
Sayyora011 <sayyoravadildorakosimova21@gmail.com>
2024-06-07 18:05:01 +0500

    Add files via upload

---
+++ 3D_Kub.py
@@ -0,0 +1,95 @@
+import pygame
+from pygame.locals import *
+from OpenGL.GL import *
+from OpenGL.GLU import *
+import random
+
+vertices = (
+    (1, -1, -1),
+    (1, 1, -1),
+    (-1, 1, -1),
+    (-1, -1, -1),
+    (1, -1, 1),
+    (1, 1, 1),
+    (-1, -1, 1),
+    (-1, 1, 1)
+)
+
+edges = (
+    (0, 1),
+    (1, 2),
+    (2, 3),
+    (3, 0),
+    (4, 5),
+    (5, 6),
+    (6, 7),
+    (7, 4),
+    (0, 4),
+    (1, 5),
+    (2, 6),
+    (3, 7)
+)
+
+colors = [
+    (1, 0, 0),
+    (0, 1, 0),
+    (0, 0, 1),
+    (1, 1, 0),
+    (1, 0, 1),
+    (0, 1, 1),
+    (0.5, 0.5, 0.5),
+    (1, 1, 1)
+]
+
+def Cube():
+    glBegin(GL_LINES)
+    for edge in edges:
+        for vertex in edge:
+            glVertex3fv(vertices[vertex])
+    glEnd()
+
+def DrawColoredCube():
+    glBegin(GL_LINES)
+    for edge in edges:
+        for vertex in edge:
+            glColor3fv(colors[vertex])
+            glVertex3fv(vertices[vertex])
+    glEnd()
+
+def RandomColorCube():
+    glBegin(GL_LINES)
+    for edge in edges:
+        glColor3fv((random.random(), random.random(), random.random()))
+        for vertex in edge:
+            glVertex3fv(vertices[vertex])
+    glEnd()
+
+def main():
+    pygame.init()
+    display = (800, 600)
+    pygame.display.set_mode(display, DOUBLEBUF | OPENGL)
+    gluPerspective(45, (display[0] / display[1]), 0.1, 50.0)
+    glTranslatef(0.0, 0.0, -10)
+    glRotatef(45, 1, 1, 1)
+
+    clock = pygame.time.Clock()
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+
+        glRotatef(1, 3, 1, 1)
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+
+        # Draw random colored cube
+        RandomColorCube()
+
+        pygame.display.flip()
+        pygame.time.wait(10)
+        clock.tick(60)
+
+if __name__ == "__main__":
+    main()
+
